b7a6a3d89653d324e1f2806d597cbe93f269a658
Gourav Kumar <33678627+unpredictablegourav@users.noreply.github.com>
2019-09-22 16:26:47 +0530

    Add files via upload

---
+++ AzureUserRole_Tested.ps1
@@ -0,0 +1,19 @@
+# Author of this script :- Gourav Kumar
+# Feel free to use and Edit, no copyrights
+# Can reach me at gouravrathore23@gmail.com or gouravin@outlook.com
+# Do not forget to share feedback and Rating on it. :)
+
+# Login to Azure Account
+#Login-AzureRmAccount
+
+# Prompting the user to select the subscription
+Get-AzureRmSubscription | Out-GridView -OutputMode Single -Title "Please select a subscription" | ForEach-Object {$SubscriptionName = $PSItem.Name}
+Write-Host "You have selected the subscription: $SubscriptionName. Proceeding with fetching the Azure User Role for it. `n" -ForegroundColor yellow
+
+# Setting the selected subscription
+Select-AzureRmSubscription -SubscriptionName $SubscriptionName
+
+# Running command to export the user role in CSV with 
+Get-AzureRmRoleAssignment | select-object -Property DisplayName, SignInName, RoleDefinitionName, ObjectType, CanDelegate | Export-Csv C:\Temp\$SubscriptionName.csv
+
+Write-Host "Sucessfully exported user role for subscription" $SubscriptionName
