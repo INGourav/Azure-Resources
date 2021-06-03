1b1eda8a8e267425c0c853fe7acf0b4abed98f85
GouravIN <gouravin@outlook.com>
2021-06-03 11:18:19 +0530

    adding all other fields v1.3

--- Vnet_Subnet_Info.ps1
+++ Vnet_Subnet_Info.ps1
@@ -10,7 +10,10 @@ foreach ($vnet in $vnets) {
         $subnetroute = $subnetrouteid.Split("/")[-1]
         $subnetendpoint = $subnet.ServiceEndpoints.Service
         $subnetendpointlocation = $subnet.ServiceEndpoints.Locations
-        Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name | Select-Object @{n="VNet Name";e={$vnet.Name -join ","}}, @{n="VNet Range";e={$vnetrange -join ","}}, @{n="Subnet Name";e={$subnet.Name -join ","}}, @{n="Subnet Address";e={$_.AddressPrefix -join ","}}, @{n="Subnet NSG";e={$subnetnsg -join ","}}, @{n="Subnet Route Name";e={$subnetroute -join ","}}, @{n="Subnet Endpoints";e={$subnetendpoint -join ","}}, @{n="Subnet Endpoints Locations";e={$subnetendpointlocation -join "; "}} | Export-Csv C:\Temp\SubnetInfo.csv -Append -NoTypeInformation
+        Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name | Select-Object @{n="VNet Name";e={$vnet.Name -join ","}}, @{n="VNet Range";e={$vnetrange -join ","}}, `
+         @{n="Subnet Name";e={$subnet.Name -join ","}}, @{n="Subnet Address";e={$_.AddressPrefix -join ","}}, @{n="Subnet NSG";e={$subnetnsg -join ","}}, @{n="Subnet Route Name";e={$subnetroute -join ","}}, `
+         @{n="Subnet Endpoints";e={$subnetendpoint -join ","}}, @{n="Subnet Endpoints Locations";e={$subnetendpointlocation -join "; "}} `
+          | Export-Csv C:\Temp\SubnetInfo.csv -Append -NoTypeInformation
         $subnetnsg = $null; $subnetroute = $null; $subnetendpoint = $null; $subnetendpointlocation = $null
     }
 }
