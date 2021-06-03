c95a00e69657d79bee58297b667c7beb91bbe3ab
GouravIN <gouravin@outlook.com>
2021-06-03 12:45:01 +0530

    adding all other fields v2.0

--- Vnet_Subnet_Info.ps1
+++ Vnet_Subnet_Info.ps1
@@ -17,3 +17,37 @@ foreach ($vnet in $vnets) {
         $subnetnsg = $null; $subnetroute = $null; $subnetendpoint = $null; $subnetendpointlocation = $null
     }
 }
+
+
+############################### Script to print value of console #################################
+
+$ErrorActionPreference = "SilentlyContinue"
+$vnets = Get-AzVirtualNetwork
+foreach ($vnet in $vnets) {
+    $subnets = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet
+    foreach ($subnet in $subnets) {
+        $snet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name
+        $vnetname = $vnet.Name
+        $vnetrange = $vnet.AddressSpace.AddressPrefixes
+        $subnetName = $snet.Name
+        $subnetaddress = $snet.AddressPrefix
+        $subnetnsgid = $snet.NetworkSecurityGroup.Id
+        $subnetnsg = $subnetnsgid.Split("/")[-1]
+        $subnetrouteid = $subnet.RouteTable.id
+        $subnetroute = $subnetrouteid.Split("/")[-1]
+        $subnetendpoint = $subnet.ServiceEndpoints.Service
+        $subnetendpointlocation = $subnet.ServiceEndpoints.Locations
+
+        $subnetconfig = New-Object psobject
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "VNet Name" -Value $vnetname
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "VNet Range" -Value  $vnetrange
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet Name" -Value $subnetName
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet Address" -Value $subnetaddress
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet NSG" -Value $subnetnsg
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet Route" -Value $subnetroute
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet Endpoint" -Value $subnetendpoint
+        $subnetconfig | Add-Member -MemberType NoteProperty -name "Subnet Endpoint Location" -Value $subnetendpointlocation
+        $subnetconfig
+        $subnetnsg = $null; $subnetroute = $null; $subnetendpoint = $null; $subnetendpointlocation = $null
+    }
+}
