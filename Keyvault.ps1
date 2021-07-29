3f2def5c8efff230fb5f91df54bcb80634efa883
GouravIN <gouravin@outlook.com>
2021-07-29 12:51:14 +0530

    All information about key vault added

--- Keyvault.ps1
+++ Keyvault.ps1
@@ -2,7 +2,7 @@
 Script of export inforamtion about KeyVault and its connected VNET
 Author : - Gourav Kumar
 Reach Me : - gouravin@outlook.com
-Version : - 1.0.0 
+Version : - 1.0.1 
 #>
 
 $keyvaults = Get-AzKeyVault |Select-Object -ExpandProperty "VaultName"
@@ -12,16 +12,34 @@ $vault = Get-AzKeyVault -VaultName $keyvault
 $Acls = $vault.NetworkAcls.VirtualNetworkResourceIds
 if ($null -ne $Acls) {
     $vaultinfo = New-Object psobject
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Subscription ID" -Value $vault.ResourceId.Split('/')[2]
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault Name" -Value $keyvault
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault ResourceGroup" -Value $vault.ResourceGroupName
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault Location" -Value $vault.Location
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault SKU" -Value $vault.Sku
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForDeployment" -Value $vault.EnabledForDeployment
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForTemplateDeployment" -Value $vault.EnabledForTemplateDeployment
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForDiskEncryption" -Value $vault.EnabledForDiskEncryption
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForRBACAuth" -Value $vault.EnableRbacAuthorization
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Soft Delete" -Value $vault.EnableSoftDelete
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Soft Delete Retention(Days)" -Value $vault.SoftDeleteRetentionInDays
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "VNET" -Value $Acls.split('/')[-3]
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "VNET ResourceGroup" -Value $Acls.split('/')[4]
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Subnet" -Value $Acls.split('/')[-1]
     $vaultinfo | Format-List
 } else {
     $vaultinfo = New-Object psobject
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Subscription ID" -Value $vault.ResourceId.Split('/')[2]
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault Name" -Value $keyvault
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault ResourceGroup" -Value $vault.ResourceGroupName
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault Location" -Value $vault.Location
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Vault SKU" -Value $vault.Sku
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForDeployment" -Value $vault.EnabledForDeployment
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForTemplateDeployment" -Value $vault.EnabledForTemplateDeployment
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForDiskEncryption" -Value $vault.EnabledForDiskEncryption
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "EnableForRBACAuth" -Value $vault.EnableRbacAuthorization
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Soft Delete" -Value $vault.EnableSoftDelete
+    $vaultinfo | Add-Member -MemberType NoteProperty -Name "Soft Delete Retention(Days)" -Value $vault.SoftDeleteRetentionInDays
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "VNET" -Value "No VNeT Attached"
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "VNET ResourceGroup" -Value "N/A"
     $vaultinfo | Add-Member -MemberType NoteProperty -Name "Subnet" -Value "No VNeT Attached"
