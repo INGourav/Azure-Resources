384519f296bd0868b1905e21ec3abd52428c5434
GouravIN <gouravin@outlook.com>
2021-09-21 13:07:46 +0530

    adding version 2 for global access permission

--- AzureDataLakeACL.ps1
+++ AzureDataLakeACL.ps1
@@ -1,15 +1,26 @@
 <#
 Author : - Gourav Kumar
 Draft One submitted by : - Ankit Kotnala
+Vesrion : - 2.0.0
 
 How to run for access on container
-.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user'
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw-
 
 How to run for access on directory
-.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user'
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw-
 
+
+If we want create Global permission scope we have to add -scope parameter flag while running
+
+How to run for global access on container
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global
+
+How to run for global access on directory
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global
 #>
 
+
+[CmdletBinding()]
 Param(
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the Datalake storage account name")]
     [Alias('DataLake')]
@@ -30,6 +41,11 @@ Param(
     [Alias('DataLake Container Directory Name')]
     [string]$datalakecontainerdir,
 
+    [parameter (Mandatory = $false, ValueFromPipeLine = $True, HelpMessage = "Provide the level of permission scope either global or access")]
+    [Alias('Access or Global permission')]
+    [ValidateNotNullOrEmpty()]
+    [string]$scope = "access",
+
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the entity name either user or group, by default it is user")]
     [Alias('Want to run for User or Group')]
     [ValidateNotNullOrEmpty()]
@@ -38,38 +54,43 @@ Param(
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the GUID or user or a group for access")]
     [Alias('User or Group GUID')]
     [ValidateNotNullOrEmpty()]
-    [string]$guid
+    [string]$guid,
+
+    [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the level of permission")]
+    [Alias('r--, -w-, rw-, --x')]
+    [ValidateNotNullOrEmpty()]
+    [string]$permission
 
 )
 
 $adsc = New-AzStorageContext -StorageAccountName $datalakestr -StorageAccountKey $datalakestrkey
 
-if ($null -match $datalakecontainerdir) {
+if ($scope -eq "global") {
 
-    $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer).ACL
-    $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityID $guid -Permission rw- -InputObject $acl 
-    Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
+   if ($null -match $datalakecontainerdir) {
 
+       $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+       Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
+       
+   } else {
+       $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+       Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl -Path $datalakecontainerdir
+   }
 }
 else {
 
-    $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $dirname).ACL
-    $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityID $guid -Permission rw- -InputObject $acl 
-    Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl
+    if ($null -match $datalakecontainerdir) {
 
+        $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer).ACL
+        $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
+    
+    }
+    else {
+    
+        $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir).ACL
+        $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl
+    
+    }
 }
-
-
-
-
-
-#    This block is used to add user under Default permission, will modify this script later or both of these option
-#    if ($null -ne $datalakecontainerdir) {
-
-#        $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType User -EntityId $guid -Permission rw- -DefaultScope
-#        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl -Verbose
-       
-#    } else {
-#        $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission rw- -DefaultScope
-#        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl -Path $datalakecontainerdir -Verbose
-#    }
