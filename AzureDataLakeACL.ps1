8d9f02958547e59d73a5c7efb4b5d6c51ed06b19
GouravIN <gouravin@outlook.com>
2021-09-21 14:45:27 +0530

    adding version 2.0.1 to remove the permissions

--- AzureDataLakeACL.ps1
+++ AzureDataLakeACL.ps1
@@ -3,25 +3,47 @@ Author : - Gourav Kumar
 Draft One submitted by : - Ankit Kotnala
 Vesrion : - 2.0.0
 
-How to run for access on container
+------------- addition of access ---------------
+
+How to run to add access on container
 .\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw-
 
-How to run for access on directory
+How to run to add access on directory
 .\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw-
 
 
 If we want create Global permission scope we have to add -scope parameter flag while running
 
-How to run for global access on container
+How to run to add global access on container
 .\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global
 
-How to run for global access on directory
+How to run to add global access on directory
 .\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global
+
+------------- reamoval of access ---------------
+
+How to run to remove access on container
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -mode remove
+
+How to run to remove access on directory
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -mode remove
+
+
+How to run to remove global access on container
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global -mode remove
+
+How to run to remove global access on directory
+.\AzureDataLakeACL.ps1 -datalakestr 'datalake storage account' -datalakestrkey 'datalake storage account key' -datalakecontainer 'container name' -datalakecontainerdir 'directory name' -userorgroup <user|group> -guid 'guid for user' -permission rw- -scope global -mode remove
 #>
 
 
 [CmdletBinding()]
 Param(
+    [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Want to add user or remove")]
+    [Alias('add or remove')]
+    [ValidateNotNullOrEmpty()]
+    [string]$mode ="add",
+
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the Datalake storage account name")]
     [Alias('DataLake')]
     [ValidateNotNullOrEmpty()]
@@ -65,32 +87,63 @@ Param(
 
 $adsc = New-AzStorageContext -StorageAccountName $datalakestr -StorageAccountKey $datalakestrkey
 
-if ($scope -eq "global") {
+if ($mode -eq "add") {
 
-   if ($null -match $datalakecontainerdir) {
+    if ($scope -eq "global") {
 
-       $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
-       Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
-       
-   } else {
-       $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
-       Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl -Path $datalakecontainerdir
-   }
-}
+        if ($null -match $datalakecontainerdir) {
+     
+            $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+            Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
+            
+        } else {
+            $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+            Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl -Path $datalakecontainerdir
+        }
+     }
+     else {
+     
+         if ($null -match $datalakecontainerdir) {
+     
+             $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer).ACL
+             $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+             Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
+         
+         } else {
+         
+             $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir).ACL
+             $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+             Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl
+         
+         }
+     }
+} 
 else {
 
-    if ($null -match $datalakecontainerdir) {
+    if ($scope -eq "global") {
 
-        $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer).ACL
-        $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
-        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Acl $acl
-    
-    }
-    else {
-    
-        $acl = (Get-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir).ACL
-        $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
-        Update-AzDataLakeGen2Item -Context $adsc -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl
-    
-    }
+        if ($null -match $datalakecontainerdir) {
+     
+            $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+            Remove-AzDataLakeGen2AclRecursive -FileSystem $datalakecontainer -Acl $acl -Context $adsc
+            
+        } else {
+            $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityId $guid -Permission $permission -DefaultScope
+            Remove-AzDataLakeGen2AclRecursive -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl -Context $adsc
+        }
+     }
+     else {
+     
+         if ($null -match $datalakecontainerdir) {
+     
+             $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+             Remove-AzDataLakeGen2AclRecursive -FileSystem $datalakecontainer -Acl $acl -Context $adsc
+         
+         } else {
+         
+             $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType $userorgroup -EntityID $guid -Permission $permission -InputObject $acl 
+             Remove-AzDataLakeGen2AclRecursive -FileSystem $datalakecontainer -Path $datalakecontainerdir -Acl $acl -Context $adsc 
+         
+         }
+     }
 }
