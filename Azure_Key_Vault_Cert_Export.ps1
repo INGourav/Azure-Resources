02731ba2561d62a4b51937356a7bf6de063fe8de
GouravIN <gouravin@outlook.com>
2022-03-16 13:50:56 +0530

    Direct import in the local machine added

--- Azure_Key_Vault_Cert_Export.ps1
+++ Azure_Key_Vault_Cert_Export.ps1
@@ -4,7 +4,7 @@ This export the given certificate from the Azure KeyVault with its key value pai
 Currently this script exports the given one certificate in a single run however I am working on version 2 that can export all certificate in single run
 
 How to run,
-.\Azure_Key_Vault_Cert_Export.ps1 -vaultname 'azkeyvault23' -certname 'azkeyvault23cert' -pwd 'P@s$w0rD' -dir 'C:\temp\'
+.\Azure_Key_Vault_Cert_Export.ps1 -vaultname 'azkeyvault23' -certname 'azkeyvault23cert' -certpwd 'P@s$w0rD' -dir 'C:\temp\'
 
 Version = 1.0.0
 #>
@@ -23,7 +23,7 @@ Param(
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the password for the certificate to use")]
     [Alias('Password for the PFX certificate file')]
     [ValidateNotNullOrEmpty()]
-    [string]$pwd,
+    [string]$certpwd,
 
 
     [parameter (Mandatory = $True, ValueFromPipeLine = $True, HelpMessage = "Provide the location to store the certicate in local machine")]
@@ -33,7 +33,7 @@ Param(
 
 )
 
-$certpwd = ConvertTo-SecureString $pwd -AsPlainText -Force
+$securecertpwd = ConvertTo-SecureString $certpwd -AsPlainText -Force
 $cert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certname
 $secret = Get-AzKeyVaultSecret -VaultName $vaultName -Name $cert.Name
 $secretValueText = '';
@@ -48,11 +48,11 @@ $secretByte = [Convert]::FromBase64String($secretValueText)
 $x509Cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
 $x509Cert.Import($secretByte, "", "Exportable,PersistKeySet")
 $type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx
-$pfxFileByte = $x509Cert.Export($type, $certpwd)
+$pfxFileByte = $x509Cert.Export($type, $securecertpwd)
 
 # This line will store the file in the system's location
 [System.IO.File]::WriteAllBytes($dir + $cert.Name + '.pfx', $pfxFileByte)
 
 # If you directly want to import this on local machine than you can uncomment below lines
 # $file = $dir + $cert.Name + '.pfx'
-# Import-PfxCertificate -FilePath $file -CertStoreLocation Cert:\CurrentUser\My -Password $certpwd
+# Import-PfxCertificate -FilePath $file -CertStoreLocation Cert:\CurrentUser\My -Password $securecertpwd
