38ed6a54e6683a5329cf200d6b8c1b0f9850fab0
GouravIN <gouravin@outlook.com>
2022-04-15 14:23:49 +0530

    block added in KeyVaultSecretUsingSAS

--- KeyVaultSecretUsingSAS.ps1
+++ KeyVaultSecretUsingSAS.ps1
@@ -27,15 +27,19 @@ Param(
     [string]$azsub
 
 )
+begin {
 
-$DeploymentOutputs = @{}
-Set-AzContext -Subscription $azsub
-$context = (Get-AzStorageAccount -ResourceGroupName $rg -Name $storageaccount).Context
-$sas = New-AzStorageAccountSASToken -Context $context -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission racwdlup
-$Secret = ConvertTo-SecureString -String $sas -AsPlainText -Force
-$Expires = (Get-Date).AddYears(2).ToUniversalTime()
-$NBF =(Get-Date).ToUniversalTime()
-$Tags = @{ "Created By" = "Gourav"; "CostCenter" = "23"}
-$ContentType = "txt" # optional
-Set-AzKeyVaultSecret -VaultName $keyvault -Name $secretname -SecretValue $Secret -Expires $Expires -NotBefore $NBF -ContentType $ContentType -Tags $Tags  # we we want to disable the secret while creation use -Disable flag
-$DeploymentOutputs['Expire'] = $Expires
+    $DeploymentOutputs = @{}
+    Set-AzContext -Subscription $azsub
+    $context = (Get-AzStorageAccount -ResourceGroupName $rg -Name $storageaccount).Context
+    $sas = New-AzStorageAccountSASToken -Context $context -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission racwdlup
+    $Secret = ConvertTo-SecureString -String $sas -AsPlainText -Force
+    $Expires = (Get-Date).AddYears(2).ToUniversalTime()
+    $NBF =(Get-Date).ToUniversalTime()
+    $Tags = @{ "Created By" = "Gourav"; "CostCenter" = "23"}
+    $ContentType = "txt" # optional
+    Set-AzKeyVaultSecret -VaultName $keyvault -Name $secretname -SecretValue $Secret -Expires $Expires -NotBefore $NBF -ContentType $ContentType -Tags $Tags  # we we want to disable the secret while creation use -Disable flag
+    $DeploymentOutputs['Expire'] = $Expires
+}
+
+end {}
