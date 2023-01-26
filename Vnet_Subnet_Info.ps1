618f4a6742ebbf7c92e2033ff0da2320e2fd8b11
Gourav Kumar <33678627+INGourav@users.noreply.github.com>
2023-01-26 12:19:27 +0000

    Updated for all subscription in same tenant

--- Vnet_Subnet_Info.ps1
+++ Vnet_Subnet_Info.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = "SilentlyContinue"
-$vnets = Get-AzVirtualNetwork
-foreach ($vnet in $vnets) {
+$subscriptions = Get-AzSubscription | select -ExpandProperty SubscriptionId
+foreach ($subscription in $subscriptions) {
+    Set-Azcontext -SubscriptionId $subscription
+    $vnets = Get-AzVirtualNetwork
+    foreach ($vnet in $vnets) {
     $subnets = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet
     foreach ($subnet in $subnets) {
         $vnetrange = $vnet.AddressSpace.AddressPrefixes
@@ -10,12 +13,13 @@ foreach ($vnet in $vnets) {
         $subnetroute = $subnetrouteid.Split("/")[-1]
         $subnetendpoint = $subnet.ServiceEndpoints.Service
         $subnetendpointlocation = $subnet.ServiceEndpoints.Locations
-        Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name | Select-Object @{n = "VNet Name"; e = { $vnet.Name -join "," } }, @{n = "VNet Range"; e = { $vnetrange -join "," } }, `
+        Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name | Select-Object @{n = "SubscriptionId"; e = { $subscription -join "," } }, @{n = "VNet Name"; e = { $vnet.Name -join "," } }, @{n = "VNet Range"; e = { $vnetrange -join "," } }, `
         @{n = "Subnet Name"; e = { $subnet.Name -join "," } }, @{n = "Subnet Address"; e = { $_.AddressPrefix -join "," } }, @{n = "Subnet NSG"; e = { $subnetnsg -join "," } }, @{n = "Subnet Route Name"; e = { $subnetroute -join "," } }, `
         @{n = "Subnet Endpoints"; e = { $subnetendpoint -join "," } }, @{n = "Subnet Endpoints Locations"; e = { $subnetendpointlocation -join "; " } } `
         | Export-Csv C:\Temp\SubnetInfo.csv -Append -NoTypeInformation
         $subnetnsg = $null; $subnetroute = $null; $subnetendpoint = $null; $subnetendpointlocation = $null
     }
+ }
 }
 
 
