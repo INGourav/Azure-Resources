92c7c8b5b1ad60e315da1837e64322fc63f97e98
Gourav Kumar <33678627+INGourav@users.noreply.github.com>
2023-02-14 14:50:01 +0000

    Update outputs.tf

--- Terraform/VNet with RouteTable/outputs.tf
+++ Terraform/VNet with RouteTable/outputs.tf
@@ -1,25 +1,25 @@
-output "nsg_id" {
-    value = azurerm_network_security_group.nsg.id
-}
-
-output "nsg_name" {
-    value = azurerm_network_security_group.nsg.name
-}
-
-output "name" {
-  value = {
-      for id in keys(var.subnet_name) : id => azurerm_subnet.hubsubnets[id].id
-  }
-}
-
 output "vnetname_out" {
     value = azurerm_virtual_network.vnet.name
 }
 
-output "location" {
+output "vnet_location" {
     value = azurerm_virtual_network.vnet.location
 }
 
-output "addresspace" {
+output "vnet_addresspace" {
     value = azurerm_virtual_network.vnet.address_space
+}
+    
+output "subnet_name" {
+  value = {
+      for id in keys(var.subnet_name) : id => azurerm_subnet.hubsubnets[id].id
+  }
+}
+    
+output "nsg_name" {
+    value = azurerm_network_security_group.nsg.name
+}
+    
+output "nsg_id" {
+    value = azurerm_network_security_group.nsg.id
 }
