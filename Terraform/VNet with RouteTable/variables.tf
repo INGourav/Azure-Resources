947e8e1b3542f9a90a2824ca9074d3ddff02b748
Gourav Kumar <33678627+INGourav@users.noreply.github.com>
2023-02-14 14:48:06 +0000

    Update variables.tf

--- Terraform/VNet with RouteTable/variables.tf
+++ Terraform/VNet with RouteTable/variables.tf
@@ -1,58 +1,70 @@
-variable "resource-group-name" {
-    type = string
-    description = "Name of the Virtual Network ResourceGroup"
-    default = "demovnetrg"
+variable "resourcegroupname" {
+  type        = string
+  description = "Name of the Virtual Network ResourceGroup"
+  default     = "demovnetrg"
 }
 
 variable "vnetname" {
-    type = string
-    description = "Name of the Virtual Network"
-    default = "demovnet"
+  type        = string
+  description = "Name of the Virtual Network"
+  default     = "demovnet"
 }
 
 variable "location" {
-    type = string
-    description = "The location of the deployment"
-    default = "uk south"
-}
-
-variable "rgname" {
-    type = string
-    description = "The name of resource group"
-    default = "routetabletestrg"
+  type        = string
+  description = "The location of the deployment"
+  default     = "uk south"
 }
 
 variable "cidr" {
-    type = list(string)
-    description = "CIDR block of the Vnet"
-    default = ["10.0.0.0/16"]
+  type        = list(string)
+  description = "CIDR block of the Vnet"
+  default     = ["10.0.0.0/16"]
 }
 
 variable "subnet_name" {
-type = map(object({
-name = string
-cidr = list(string)
-}))
-default = {
-"subnet1" = {
-name = "subnet1"
-cidr = ["10.0.1.0/24"]
-},
-"subnet2" = {
-name = "subnet2"
-cidr = ["10.0.2.0/24"]
-}
-}
+  type = map(object({
+    name = string
+    cidr = list(string)
+  }))
+  default = {
+    "subnet1" = {
+      name = "subnet1"
+      cidr = ["10.0.1.0/24"]
+    },
+    "subnet2" = {
+      name = "subnet2"
+      cidr = ["10.0.2.0/24"]
+    }
+  }
 }
 
 variable "nsgname" {
-    type = string
-    description = "NSG name for subnet"
-    default = "demonsg"
+  type        = string
+  description = "NSG name for subnet"
+  default     = "demonsg"
 }
 
 variable "routetablename" {
-    type = string
-    description = "NSG name for route table"
-    default = "demort"
+  type        = string
+  description = "NSG name for route table"
+  default     = "demort"
+}
+
+variable "routename" {
+  type        = string
+  description = "Name of the route for route table"
+  default     = "route1"
+}
+
+variable "routeaddressprefix" {
+  type        = string
+  description = "Address prefix for route table"
+  default     = "10.1.0.0/16"
+}
+
+variable "nexthop" {
+  type        = string
+  description = "Next hop for route table"
+  default     = "VnetLocal"
 }
