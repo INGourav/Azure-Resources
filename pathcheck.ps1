8136335095c06083cffc5025b9a3b4491e61e97c
Gourav Kumar <33678627+INGourav@users.noreply.github.com>
2024-02-05 18:58:08 +0530

    Update pathcheck.ps1

--- pathcheck.ps1
+++ pathcheck.ps1
@@ -1,18 +1,21 @@
 # Set the paths
-$originalFolder = "C:\inetpub\wwwroot"
-$newFolder = "C:\inetpub\wwwrootNEW"
-$oldFolder = "C:\inetpub\wwwrootOLD"
-$old_name ="wwwrootOLD"
-$original_folder_name = "wwwroot"
-$path = "C:\inetpub\wwwroot"
- 
-Test-Path -path $path
- 
-# Create a copy of the original folder
-Copy-Item -Path $originalFolder -Destination $newFolder -Recurse
- 
-# Rename the original folder to "wwwrootOLD"
-Rename-Item -Path $originalFolder -NewName $old_name
- 
-# Rename the new folder to "wwwroot"
-Rename-Item -Path $newFolder -NewName $original_folder_name
+$originalFolder = "G:\PowerShell\testing\path\"
+$newFolder = "G:\PowerShell\testing\pathNEW"
+$old_name ="pathOLD"
+$original_folder_name = "pathold"
+
+# Check path
+$testpath = Test-Path -path $originalFolder
+if ($true -eq $testpath) {
+    # Create a copy of the original folder
+    Copy-Item -Path $originalFolder -Destination $newFolder -Recurse
+    Start-Sleep -Seconds 5
+    
+    # Rename the original folder to "wwwrootOLD"
+    Rename-Item -Path $originalFolder -NewName $old_name
+     
+    # Rename the new folder to "wwwroot"
+    Rename-Item -Path $newFolder -NewName $original_folder_name
+}else {
+    Write-Output "Path not found"
+}
